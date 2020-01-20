e1509dbe0f6150a04754cfc220ea53a978294705
MENG-YEN TSAI <37220246+kkoo1122@users.noreply.github.com>
2020-01-20 11:33:12 -0700

    Update matchfile.py

--- matchfile.py
+++ matchfile.py
@@ -8,16 +8,16 @@ from IPython import embed
 mode = 'copy'
 
 # target path
-path_target = '/auto/shared/client_data/wada/potato_images/raws/yellow_raws_dec_2019/'
+path_target = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/spatial_data/gtFine/val/'
 
 # move target to direction
 move_dir_target = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/Work/potato_original/'
 
 # target2 path
-path_target2 = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/Work/potato_masks/ok_using/'
+path_target2 = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/Work/potato_original/'
 
 # move target2 to direction
-move_dir_target2 ='/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/images_new/gtFine/train/'
+move_dir_target2 ='/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/spatial_data/leftImg8bit/val/'
 
 filename_image = os.listdir(path_target)
 filename_image.sort()
@@ -29,13 +29,13 @@ for index, f in enumerate(filename_image):
     if f in filename_label:
         print(index+1, f)
         if mode == 'copy':
-            shutil.copyfile(path_target+f, move_dir_target+f)
-            #shutil.copyfile(path_target2+f, move_dir_target2+f)
-            print("File match completed! (copy)")
+            #shutil.copyfile(path_target+f, move_dir_target+f)
+            shutil.copyfile(path_target2+f, move_dir_target2+f)
         elif mode == 'move':
             shutil.move(path_target+f, move_dir_target+f)
             #shutil.move(path_target2+f, move_dir_target2+f)
-            print("File match completed! (move)")
         else:
             print("Please choose correct mode. (copy/move)")
             break
+            
+print("File match completed! ({})".format(mode))
