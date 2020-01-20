7ed2373f9680756e1778399055c65023715e5ff7
MENG-YEN TSAI <37220246+kkoo1122@users.noreply.github.com>
2020-01-20 11:23:49 -0700

    Update matchfile.py

--- matchfile.py
+++ matchfile.py
@@ -5,13 +5,19 @@ import shutil
 
 from IPython import embed
 
-path_train = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/images_new/leftImg8bit/train/'
+mode = 'copy'
 
-move_dir_train = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/Work/leftImg8bit/train/'
+# original image path
+path_train = '/auto/shared/client_data/wada/potato_images/raws/yellow_raws_dec_2019/'
 
-path_label = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/Work/gtFine/train/'
+# original image direction
+move_dir_train = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/Work/potato_original/'
 
-move_dir_label ='/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/images_new/gtnotFine/train/'
+# label path
+path_label = '/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/Work/potato_masks/ok_using/'
+
+# label direction
+move_dir_label ='/media/kevintsai/cef8b40c-14d2-4a8f-a359-9da3eeceb1d4/Projects/potato/images_new/gtFine/train/'
 
 filename_image = os.listdir(path_train)
 filename_image.sort()
@@ -22,7 +28,14 @@ filename_label.sort()
 for index, f in enumerate(filename_image):
     if f in filename_label:
         print(index+1, f)
-        #shutil.copyfile(path_train+f, move_dir_train+f)
-        shutil.copyfile(path_label+f, move_dir_label+f)
-
-print("File match completed!")
+        if mode == 'copy':
+            shutil.copyfile(path_train+f, move_dir_train+f)
+            #shutil.copyfile(path_label+f, move_dir_label+f)
+            print("File match completed! (copy)")
+        elif mode == 'move':
+            shutil.move(path_train+f, move_dir_train+f)
+            #shutil.move(path_label+f, move_dir_label+f)
+            print("File match completed! (move)")
+        else:
+            print("Please choose correct mode. (copy/move)")
+            break
